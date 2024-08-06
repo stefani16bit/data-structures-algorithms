603236a02764230957430c6854acdfb953cc7de6
Stefani Marchi <99232385+stefanimarchi@users.noreply.github.com>
2024-08-05 21:18:14 -0300

    binary search

---
+++ Algorithms/binary-search.py
@@ -0,0 +1,25 @@
+def binary_search():
+
+    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+    target = 23 
+
+    start = 0
+    end = len(list) - 1
+
+    while start <= end:
+        middle = (start + end) // 2
+
+        if list[middle] > target:
+            end = middle -1
+
+        elif list[middle] < target:
+            start = middle +1
+
+        else:
+            return middle
+                
+print(binary_search())
+        
+
+    
